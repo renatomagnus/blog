bc0a058427280e81a695c04a84f65e62ec1340ee
rmagnusr <rmagnusdarosa@gmail.com>
2023-11-10 23:45:15 -0300

    documentations

--- src/components/knowledge/index.js
+++ src/components/knowledge/index.js
@@ -11,45 +11,77 @@ const Knowledge = () => {
       <div className="knowledge-items">
         <div className="knowledge-column">
           <h3 className="column-name">Estruture</h3>
-          <div className="knowledge-item">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img src="icons/html.png" alt="" />
             <p className="item-name knowledge-margin-b">Html</p>
-          </div>
+          </a>
           <h3 className="column-name">Tests</h3>
-          <div className="knowledge-item">
+          <a
+            href="https://docs.cypress.io/guides/overview/why-cypress"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img className="scale08" src="icons/cypress.png" alt="" />
             <p className="item-name">Cypress</p>
-          </div>
+          </a>
         </div>
         <div className="knowledge-column">
           <h3 className="column-name">Design</h3>
-          <div className="knowledge-item">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img src="icons/css.png" alt="" />
             <p className="item-name">Css</p>
-          </div>
-          <div className="knowledge-item">
+          </a>
+          <a
+            href="https://sass-lang.com/documentation/"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img src="icons/sass.png" alt="" />
             <p className="item-name">Sass</p>
-          </div>
-          <div className="knowledge-item">
+          </a>
+          <a
+            href="https://lesscss.org/"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img src="icons/less.png" alt="" />
             <p className="item-name">Less</p>
-          </div>
+          </a>
         </div>
         <div className="knowledge-column">
           <h3 className="column-name">Behavior</h3>
-          <div className="knowledge-item">
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img className="scale08" src="/icons/javascript.png" alt="" />
             <p className="item-name">JavaScript</p>
-          </div>
-          <div className="knowledge-item">
+          </a>
+          <a
+            href="https://react.dev/blog/2023/03/16/introducing-react-dev"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img src="icons/react.png" alt="" />
             <p className="item-name">React.js</p>
-          </div>
-          <div className="knowledge-item">
+          </a>
+          <a
+            href="https://nextjs.org/docs"
+            target="_blank"
+            className="knowledge-item"
+          >
             <img src="icons/next.png" alt="" />
             <p className="item-name">Next.js</p>
-          </div>
+          </a>
         </div>
       </div>
     </section>
