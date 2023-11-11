82f2dbf8df3bc58805c6e85950adc49071731272
rmagnusr <rmagnusdarosa@gmail.com>
2023-11-11 00:41:50 -0300

    social medias

---
+++ src/components/socialmedias/index.js
@@ -0,0 +1,17 @@
+const SocialMedias = () => {
+  return (
+    <aside className="social-medias">
+      <div className="social-medias-icons">
+        <a href="https://github.com/renatomagnus" target="_blank">
+          <img src="social-medias/github.png" alt="Github" />
+        </a>
+        <a href="https://www.linkedin.com/in/renato-magnus/" target="_blank">
+          <img src="social-medias/linkedin.png" alt="Linkedin" />
+        </a>
+      </div>
+      <div className="social-medias-line"></div>
+    </aside>
+  );
+};
+
+export default SocialMedias;
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,14 +2,16 @@ import AboutMe from '@/components/aboutme';
 import Family from '@/components/family';
 import Header from '@/components/header';
 import Knowledge from '@/components/knowledge';
+import SocialMedias from '@/components/socialmedias';
 
 const About = () => {
   return (
     <>
       <Header />
       <AboutMe />
-      <Knowledge/>
-      <Family/>
+      <Knowledge />
+      <Family />
+      <SocialMedias />
     </>
   );
 };
