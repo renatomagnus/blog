3dd1c67f4ce7440d9189f10faf4f7873e9809be7
rmagnusr <rmagnusdarosa@gmail.com>
2023-11-12 14:26:12 -0300

    posts adjustments

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -6,7 +6,7 @@ const Posts = () => {
           <p>Escrevo artigos para parecer inteligente</p>
         </div>
         <div className="posts-items">
-          <div className="post-item">
+          <a href="#" className="post-item">
             <h2>Título</h2>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
@@ -14,8 +14,12 @@ const Posts = () => {
               itaque delectus minus sapiente eum qui nam, possimus nesciunt?
               Voluptas ipsa dolores commodi quod?
             </p>
-          </div>
-          <div className="post-item">
+            <div className="post-footer">
+              <p className="post-theme">CSS</p>
+              <p className="post-date">11/12/23</p>
+            </div>
+          </a>
+          <a href="#" className="post-item">
             <h2>Título</h2>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
@@ -23,8 +27,12 @@ const Posts = () => {
               itaque delectus minus sapiente eum qui nam, possimus nesciunt?
               Voluptas ipsa dolores commodi quod?
             </p>
-          </div>
-          <div className="post-item">
+            <div className="post-footer">
+              <p className="post-theme">CSS</p>
+              <p className="post-date">11/12/23</p>
+            </div>
+          </a>
+          <a href="#" className="post-item">
             <h2>Título</h2>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
@@ -32,8 +40,12 @@ const Posts = () => {
               itaque delectus minus sapiente eum qui nam, possimus nesciunt?
               Voluptas ipsa dolores commodi quod?
             </p>
-          </div>
-          <div className="post-item">
+            <div className="post-footer">
+              <p className="post-theme">CSS</p>
+              <p className="post-date">11/12/23</p>
+            </div>
+          </a>
+          <a href="#" className="post-item">
             <h2>Título</h2>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
@@ -41,8 +53,12 @@ const Posts = () => {
               itaque delectus minus sapiente eum qui nam, possimus nesciunt?
               Voluptas ipsa dolores commodi quod?
             </p>
-          </div>
-          <div className="post-item">
+            <div className="post-footer">
+              <p className="post-theme">CSS</p>
+              <p className="post-date">11/12/23</p>
+            </div>
+          </a>
+          <a href="#" className="post-item">
             <h2>Título</h2>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
@@ -50,44 +66,11 @@ const Posts = () => {
               itaque delectus minus sapiente eum qui nam, possimus nesciunt?
               Voluptas ipsa dolores commodi quod?
             </p>
-          </div>
-          <div className="post-item">
-            <h2>Título</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
-              assumenda, quaerat accusantium fuga consectetur voluptatibus aut
-              itaque delectus minus sapiente eum qui nam, possimus nesciunt?
-              Voluptas ipsa dolores commodi quod?
-            </p>
-          </div>
-          <div className="post-item">
-            <h2>Título</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
-              assumenda, quaerat accusantium fuga consectetur voluptatibus aut
-              itaque delectus minus sapiente eum qui nam, possimus nesciunt?
-              Voluptas ipsa dolores commodi quod?
-            </p>
-          </div>
-          <div className="post-item">
-            <h2>Título</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
-              assumenda, quaerat accusantium fuga consectetur voluptatibus aut
-              itaque delectus minus sapiente eum qui nam, possimus nesciunt?
-              Voluptas ipsa dolores commodi quod?
-            </p>
-          </div>
-          <div className="post-item">
-            <h2>Título</h2>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora,
-              assumenda, quaerat accusantium fuga consectetur voluptatibus aut
-              itaque delectus minus sapiente eum qui nam, possimus nesciunt?
-              Voluptas ipsa dolores commodi quod?
-            </p>
-          </div>
-          
+            <div className="post-footer">
+              <p className="post-theme">CSS</p>
+              <p className="post-date">11/12/23</p>
+            </div>
+          </a>
         </div>
         <div className="cat-articles">
           <p>Espero que tenha funcionado</p>
