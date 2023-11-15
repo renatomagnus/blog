1ada2ba36282df83ecdc17d93eb9a2fdeb6f5031
rmagnusr <rmagnusdarosa@gmail.com>
2023-11-15 13:31:11 -0300

    i18n en finished

--- src/pages/posts/how-to-study-and-consume-programming-contents/index.js
+++ src/pages/posts/how-to-study-and-consume-programming-contents/index.js
@@ -1,71 +1,56 @@
 import Header from '@/components/header';
 import SocialMedias from '@/components/socialmedias';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 
 const HowToStudyAndConsumeProgrammingContents = () => {
+  const { t, i18n } = useTranslation('how-to-study-and-consume-programming-contents','common');
+
   return (
     <>
       <Header />
       <article className="article">
         <h1 className="article-title">
-          How To Study and Consume Programming Contents
+          {t("article.title")}
         </h1>
         <p className="article-paragraph">
-          When I started programming, I think "I will go deep on this, be
-          surrounded by tech subjects 24/7 and I will learn fast and better than
-          others Jr's that compete with me.{' '}
+        {t("article.text1")}
         </p>
         <p className="article-paragraph">
-          Damn, I was totally wrong. Listen to a podcast or youtube video while
-          we clean our house isn't study! Doesn't matter if I watch 3 hours of
-          content, I wasn't paying attention, but will have the sensation that I
-          completed my studies for today.
+        {t("article.text2")}
         </p>
         <p className="article-paragraph">
-          When I reallized all that, I stopped immediatly. The shower now is
-          time to relax, reestart my brain for my studies that will happen after
-          shower. Before, I shower listenning to podcast and don't have the
-          willing to hard study after, my brain was always tired.
+        {t("article.text3")}
         </p>
         <p className="article-paragraph">
-          In the same time, one thing that I always listen in this contents was
-          that we learn from practice. So I challenged my self: "can I READ
-          contents from blogs and forums, and practice personal projects
-          literally every day?"
+        {t("article.text4")}
         </p>
         <p className="article-paragraph">
-          The word read is highlighted because we need to read and not watch
-          content. When we watch or (worst) just listen, we don't pay attention
-          as we should. And don't came to me saying "I always pay attention in
-          two or three things, I'm used to it", cause you aren't, X-men. You can
-          do two things at same time, but you need to realize and assume to
-          yourself that you are always giving up one for the other.
+        {t("article.text5")}
         </p>
         <p className="article-paragraph">
-          After accepted my own challenge, I saw my contributions on Github
-          happen more frequently, my learning curve has improved and I could
-          start this blog. This makes me very happy, I think that now I'm in the
-          right way, junt need keep going, consistently.
+        {t("article.text6")}
         </p>
         <p className="article-paragraph">
-          Well, I still watch youtube videos, tech content, but not exactly what
-          I need to study. I understand that this videos are only for
-          entertainment and for me be always more familiar with english and tech
-          topics overall.{' '}
+        {t("article.text7")}
         </p>
         <p className="article-paragraph">
-          So start your own projects, focus on your studies. Realize that your
-          future depends on your present and if you like programming and are in
-          the beginning of your carrer, you should know that even working on the
-          area, in five years you can still be considered a jr developer, or a
-          mediocre mid-level, and is that what you really want for yourself five
-          years from know?
+        {t("article.text8")}
         </p>
       </article>
-      <p className="article-footer">Renato Magnus 11/14/23</p>
+      <p className="article-footer">{t("article.footer")}</p>
 
       <SocialMedias />
     </>
   );
 };
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['how-to-study-and-consume-programming-contents', 'common'])),
+    },
+  };
+}
 
 export default HowToStudyAndConsumeProgrammingContents;
